e40bfcc329143235b8f727b004ff88bc1402b57a
liuyang1 <liuyang1@mail.ustc.edu.cn>
2013-12-07 23:56:15 +0800

    update 005 006

---
+++ 006.scm
@@ -0,0 +1,10 @@
+(define (square x) (* x x))
+
+(define (range start stop)
+  (if (> start stop) '()
+    (cons start (range (+ start 1) stop))))
+
+(define *seq* (range 1 100))
+
+(display (- (square (apply + *seq*)) (apply + (map square *seq*))))
+(newline)

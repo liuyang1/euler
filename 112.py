6384e5feb707cfeeb283d3ee9baa44e10a42b5f0
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-06-23 18:15:13 +0800

    add 112

---
+++ 112.py
@@ -0,0 +1,25 @@
+import itertools
+
+
+def digit(num):
+    lst = []
+    while num > 0:
+        lst.append(num % 10)
+        num /= 10
+    return lst
+
+
+def isBouncy(dig):
+    p = [(dig[i], dig[i + 1]) for i in range(len(dig) - 1)]
+    dec = all([a >= b for a, b in p])
+    inc = all([a <= b for a, b in p])
+    return not (dec or inc)
+
+p = 0.99
+cnt = 0
+for i in itertools.count(100):
+    if isBouncy(digit(i)):
+        cnt += 1
+    if cnt >= i * p:
+        print i
+        break

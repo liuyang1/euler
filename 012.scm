931e6cdeb8f954ed330f1961768c64beab93d8af
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-08-02 14:17:14 +0800

    add 012.scm

---
+++ 012.scm
@@ -0,0 +1,32 @@
+(define (triangle x) (/ (* x (+ x 1)) 2))
+
+(define (power n i)
+  (if (= i 0) 1
+    (* n (power n (- i 1)))))
+
+(define (factor n)
+  (define (hlp num f)
+    (if (= 0 (remainder num f))
+      (+ 1 (hlp (/ num f) f))
+      0))
+  (define (loop num f)
+    (if (= num 1) '()
+      (let* ((freq (hlp num f))
+             (next (loop (/ num (power f freq)) (+ f 1))))
+        (if (= freq 0)
+          next
+          (cons (cons f freq) next)))))
+  (loop n 2))
+
+(define (factor-counter lst)
+  (reduce * 1 (map (lambda (x) (+ 1 (cdr x))) lst)))
+
+(define (loop i)
+  (let* ((tri (triangle i))
+         (fac (factor tri))
+         (cnt (factor-counter fac)))
+    (if (> cnt 500)
+      (begin (display tri) (display "\t") (display cnt) (display "\t") (display fac) (newline))
+      (loop (+ i 1)))))
+
+(loop 1)

c0ec7474189b6508986ab9cff33cc38dd16a6778
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-08-09 16:31:23 +0800

    add 003.clj

---
+++ 003.clj
@@ -0,0 +1,12 @@
+(defn square [x] (* x x))
+(defn defactor [n]
+  (defn hlp [n divisor factor]
+    (if (> (square divisor) n)
+      (cons n factor)
+      (if (= 0 (mod n divisor))
+        (hlp (/ n divisor) divisor (cons divisor factor))
+        (hlp n (+ divisor 1) factor))))
+  (hlp n 2 '()))
+
+(println (defactor 13195))
+(println (defactor 600851475143))

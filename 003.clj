5986297736b459a477b34acc61a99755e3812035
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-08-09 21:28:33 +0800

    using cond instead of if for 003.clj

--- 003.clj
+++ 003.clj
@@ -1,11 +1,9 @@
 (defn square [x] (* x x))
 (defn defactor [n]
   (defn hlp [n divisor factor]
-    (if (> (square divisor) n)
-      (cons n factor)
-      (if (= 0 (rem n divisor))
-        (hlp (/ n divisor) divisor (cons divisor factor))
-        (hlp n (+ divisor 1) factor))))
+    (cond (> (square divisor) n) (cons n factor)
+          (= 0 (rem n divisor)) (hlp (/ n divisor) divisor (cons divisor factor))
+          :else (hlp n (+ divisor 1) factor)))
   (hlp n 2 '()))
 
 (println (defactor 13195))

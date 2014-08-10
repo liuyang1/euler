413c4c3f4ab83f390feada603767a721e98ba3db
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-08-10 22:00:37 +0800

    update 004.clj using partial

--- 004.clj
+++ 004.clj
@@ -1,9 +1,11 @@
-(defn digit [n base]
+(defn digit [base n]
   (if (< n base) (list n)
-    (cons (rem n base) (digit (quot n base) base))))
+    (cons (rem n base) (digit base (quot n base)))))
+
+(def digit10 (partial digit 10))
 
 (defn isPanlindrome? [n]
-  (let [num-digit (digit n 10)]
+  (let [num-digit (digit10 n)]
     (= num-digit (reverse num-digit))))
 
 (defn prod [lst]

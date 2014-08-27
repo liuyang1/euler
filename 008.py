76a1298fd6c5fc483738e94bbf1ba22bdb72b1b9
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-08-27 22:40:18 +0800

    update

--- 008.py
+++ 008.py
@@ -1,8 +1,12 @@
 import operator
+from functools import reduce
+
+
 def maxAdjMul(a, n):
     aa = [a[i:] for i in range(n)]
     aa = zip(*aa)
     return max([reduce(operator.mul, i) for i in aa])
+
 
 def getData():
     with open("data.008") as f:
@@ -12,6 +16,5 @@ def getData():
             l = [int(i) for i in l]
             lst.extend(l)
     return lst
-
 data = getData()
 print maxAdjMul(data, 13)
--- 027.py
+++ 027.py
@@ -1,8 +1,11 @@
 from common.mymath import *
 
-def quad(n,a,b):
+
+def quad(n, a, b):
     return n * n + a * n + b
-def cntPrime(a,b):
+
+
+def cntPrime(a, b):
     n = 0
     while 1:
         num = quad(n, a, b)
@@ -16,7 +19,7 @@ max = 0
 maxcom = (0, 0)
 for a in range(-1 * r, r + 1):
     for b in range(-1 * r, r + 1):
-        cnt = cntPrime(a,b)
+        cnt = cntPrime(a, b)
         if cnt > max:
             print a, b, cnt
             max = cnt
